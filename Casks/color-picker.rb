6534d6a7996c1bd5c3cb9e99d45b07128543ef13
Michael Bullington <michael@bullington.xyz>
2021-09-05 14:02:56 -0400

    Fix tap

--- Casks/color-picker.rb
+++ Casks/color-picker.rb
@@ -2,7 +2,7 @@ cask "color-picker" do
     version "1.0.0"
     sha256 :no_check
   
-    url "https://cdn.lan/macOS/Color Picker.app.zip"
+    url "https://cdn.lan/macOS/ColorPicker.app.zip"
     name "Color Picker"
   
     app "Color Picker.app"
