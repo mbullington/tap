614acb7ad210075d129ef192dcacff376f4734ec
Michael Bullington <michael@bullington.xyz>
2021-09-05 13:59:02 -0400

    add casks

---
+++ Casks/color-picker.rb
@@ -0,0 +1,9 @@
+cask "color-picker" do
+    version "1.0.0"
+    sha256 :no_check
+  
+    url "https://cdn.lan/macOS/Color Picker.app.zip"
+    name "Color Picker"
+  
+    app "Color Picker.app"
+end
---
+++ Casks/pico-8.rb
@@ -0,0 +1,9 @@
+cask "pico-8" do
+    version "0.2.1"
+    sha256 :no_check
+  
+    url "https://cdn.lan/macOS/PICO-8.app.zip"
+    name "PICO-8"
+  
+    app "PICO-8.app"
+end
