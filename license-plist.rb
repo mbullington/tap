d84ba413dcc02595d098303137ab9f06b3520901
Michael Bullington <michael.bullington@mapbox.com>
2022-03-16 21:34:03 -0400

    Add license-plist to tap

---
+++ license-plist.rb
@@ -0,0 +1,12 @@
+class LicensePlist < Formula
+    desc "A license list generator of all your dependencies for iOS applications"
+    homepage ""
+    url "https://github.com/mono0926/LicensePlist/archive/3.19.1.tar.gz"
+    sha256 "508733edf362a3b999a2f660527f19bba95c43f2f5f99925cb553a54c3317fa8"
+
+    def install
+        system "make", "install", "PREFIX=#{prefix}"
+    end
+
+    depends_on :xcode => ["8.3", :build]
+end
